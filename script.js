cbd202277082891e82efcc354739cb838f572f9e
Nana <nanakurnianingsih82716@gmail.com>
2023-10-04 00:14:56 +0700

    fix page

--- script.js
+++ script.js
@@ -1,59 +1,70 @@
-// Tambahkan kode JavaScript kalian di file ini
-
-// mengakses input & me return object
+// 6. Membaca isi dari inputan
 function handleGetFormData() {
-    return {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      city: document.getElementById("city").value,
-      zipCode: document.getElementById("zip-code").value,
-      status: document.getElementById("status").checked
-    }
+  const name = document.getElementById("name").value;
+  const city = document.getElementById("city").value;
+  const email = document.getElementById("email").value;
+  const zipCode = document.getElementById("zip-code").value;
+  const status = document.getElementById("status").checked;
+
+  return {
+    name,
+    city,
+    email,
+    zipCode,
+    status
+  };
+}
+
+// 7. Validasi zip-code
+function isNumber(value) {
+  return !isNaN(value);
+}
+
+// 8. Validasi checkbox
+function checkboxIsChecked() {
+  return document.getElementById("status").checked;
+}
+
+// 9. Validasi semua inputan
+function validateFormData(formData) {
+  if (
+    formData &&
+    isNumber(formData.zipCode) &&
+    formData.status === true
+  ) {
+    return true;
+  } else {
+    return false;
   }
-  
-  // mengecek isi input zipCode
-  function isNumber(string) {
-    return !isNaN(string) ? true : false;
+}
+
+// 10. Proses submit
+function submit(event) {
+  event.preventDefault();
+  const formData = handleGetFormData();
+
+  if (!validateFormData(formData)) {
+    const warningDiv = document.getElementById("warning");
+    warningDiv.textContent = "Periksa form anda sekali lagi";
+    warningDiv.style.color = "red";
+    warningDiv.style.fontSize = "14px";
+
+    // Menghilangkan pesan setelah 3 detik
+    setTimeout(() => {
+      warningDiv.textContent = "";
+    }, 3000);
+
+  } else {
+    const warningDiv = document.getElementById("warning");
+    warningDiv.textContent = "";
+    // Lakukan proses submit data
+
+
+    // Reset inputan pada label
+    form.reset();
   }
-  
-  // function mengecek isi status
-  function checkboxIsChecked() {
-    const formData = handleGetFormData();
-    return formData.status === true ? true : false;
-  }
-  
-  // function mengecek 3 fungsi diatas
-  function validateFormData(formData) {
-    const string = formData.zipCode;
-  
-    for (const key in formData) {
-      if (formData.hasOwnProperty(key) && formData[key] === "") {
-        return false;
-      }
-    }
-    return isNumber(string) && checkboxIsChecked();
-  }
-  
-  // function validasi input field
-  function submit() {
-    const formData = handleGetFormData();
-    const response = document.getElementById("warning");
-  
-    if (!validateFormData(formData)) {
-      response.textContent = "Periksa form anda sekali lagi";
-    } else {
-      response.textContent = "";
-    }
-  }
-  
-  // event handler button input
-  document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("submit-form").addEventListener(
-      "click",
-      function (event) {
-        event.preventDefault();
-        submit();
-      },
-      false
-    );
-  });
+}
+
+// Menghubungkan function submit dengan form
+const form = document.querySelector("form");
+form.addEventListener("submit", submit);
